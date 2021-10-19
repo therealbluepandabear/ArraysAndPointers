689fcea02ed6242cf8600b7142b8af9c2add67f6
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 12:43:43 +1300

    Printing out the values of the array using a 'for' loop.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,18 @@
 int main() {
     int luckyNumbers[5];
 
+    std::cout << "\n";
+
     for (int i = 0; i <= 4; i++) {
         std::cout << "Number: ";
         std::cin >> luckyNumbers[i];
     }
+
+    for (int i = 0; i <= 4; i++) {
+        std::cout << *(luckyNumbers + i) << ", ";
+    }
+
+    std::flush(std::cout);
 
     return 0;
 }
