7e18a155997d91bcb666e6b6d7d5c77197edaaae
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 12:38:25 +1300

    Deleted some code.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 
 int main() {
-    int luckyNumbers[5] = { 2, 3, 5, 7, 9 };
-    std::cout << luckyNumbers << std::endl;
-    std::cout << &luckyNumbers[0] << std::endl; // Should be the same result as above.
-    std::cout << luckyNumbers[2] << std::endl;
-    std::cout << *(luckyNumbers + 2) << std::endl;
+    int luckyNumbers[5];
 
     return 0;
 }
