dd25cf126a343eff3600a2e65d838fdb23233d37
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 12:36:04 +1300

    Finding a particular index in an array.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 int main() {
     int luckyNumbers[5] = { 2, 3, 5, 7, 9 };
     std::cout << luckyNumbers << std::endl;
+    std::cout << &luckyNumbers[0] << std::endl; // Should be the same result as above.
+    std::cout << luckyNumbers[2] << std::endl;
 
     return 0;
 }
